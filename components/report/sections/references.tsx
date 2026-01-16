cb9a2a47ced7d7df972863d6c0c58692124226f7
Jerome <nietsemorej@gmail.com>
2026-01-15 17:28:45 -0800

    refactor: Replace `<a>` tags with `<Link>` components for external references.

--- components/report/sections/references.tsx
+++ components/report/sections/references.tsx
@@ -7,9 +7,9 @@ export const References = () => (
             <li>Europa Publications. (2011). <em>The Europa World of Learning</em> (62nd). Routledge.</li>
             <li>AACRAO. (2011). <em>AACRAO Academic Record and Transcript Guide</em> (1st (2011)).</li>
             <li>Peterson's. (2020). <em>Peterson's Graduate Programs Series</em> (55th).</li>
-            <li>The Hong Kong University of Science and Technology. (n.d.). <em>Home Page</em>. (<a href="https://hkust.edu.hk" target="_blank" rel="noreferrer" className="text-blue-600 hover:underline">https://hkust.edu.hk</a>)</li>
-            <li>University of Cambridge. (n.d.). <em>Home Page</em>. (<a href="https://www.cam.ac.uk" target="_blank" rel="noreferrer" className="text-blue-600 hover:underline">https://www.cam.ac.uk</a>)</li>
-            <li>The University of Hong Kong. (n.d.). <em>Home Page</em>. (<a href="https://www.hku.hk" target="_blank" rel="noreferrer" className="text-blue-600 hover:underline">https://www.hku.hk</a>)</li>
+            <li>The Hong Kong University of Science and Technology. (n.d.). <em>Home Page</em>. (<Link href="https://hkust.edu.hk" target="_blank" rel="noreferrer" className="text-blue-600 hover:underline">https://hkust.edu.hk</Link>)</li>
+            <li>University of Cambridge. (n.d.). <em>Home Page</em>. (<Link href="https://www.cam.ac.uk" target="_blank" rel="noreferrer" className="text-blue-600 hover:underline">https://www.cam.ac.uk</Link>)</li>
+            <li>The University of Hong Kong. (n.d.). <em>Home Page</em>. (<Link href="https://www.hku.hk" target="_blank" rel="noreferrer" className="text-blue-600 hover:underline">https://www.hku.hk</Link>)</li>
         </ul>
     </div>
 )
